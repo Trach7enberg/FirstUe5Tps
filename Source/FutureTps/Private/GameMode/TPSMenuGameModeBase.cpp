2918cb512c0e148548362c79b0f366f983de9b00
my-laptop <2655667253@qq.com>
2024-06-22 19:18:01 +0800

    修改项目的GitIgnore,缩小项目大小,只上传源代码文件

---
+++ Source/FutureTps/Private/GameMode/TPSMenuGameModeBase.cpp
@@ -0,0 +1,5 @@
+// FutureTPS Game All Rights Reserved
+
+
+#include "GameMode/TPSMenuGameModeBase.h"
+
---
+++ Source/FutureTps/Public/GameMode/TPSMenuGameModeBase.h
@@ -0,0 +1,17 @@
+// FutureTPS Game All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TPSMenuGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FUTURETPS_API ATPSMenuGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
